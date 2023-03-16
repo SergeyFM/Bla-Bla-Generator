f355ed607d930a4f1dd0a5aa693c8b847fe8685b
SergeyFM <github@svistunov.su>
2023-03-16 18:06:10 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace WinFormsAppDemo
+{
+    partial class mainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            generateButton = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(53, 30);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(715, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // generateButton
+            // 
+            generateButton.Location = new Point(53, 355);
+            generateButton.Name = "generateButton";
+            generateButton.Size = new Size(104, 41);
+            generateButton.TabIndex = 1;
+            generateButton.Text = "Generate it!";
+            generateButton.UseVisualStyleBackColor = true;
+            generateButton.Click += generateButton_Click;
+            // 
+            // mainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(generateButton);
+            Controls.Add(textBox1);
+            Name = "mainForm";
+            Text = "MainForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button generateButton;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace WinFormsAppDemo
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            // Generate text here
+        }
+    }
+}
