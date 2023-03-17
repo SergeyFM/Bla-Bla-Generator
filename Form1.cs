e8456185b733e00872ce2b460f3fdf6f9f9afb07
SergeyFM <github@svistunov.su>
2023-03-17 11:03:44 +0300

    Clean up

--- Form1.cs
+++ Form1.cs
@@ -1,16 +1,10 @@
-using System.DirectoryServices.ActiveDirectory;
-
-namespace WinFormsAppDemo
-{
-    public partial class mainForm : Form
-    {
-        public mainForm()
-        {
+namespace WinFormsAppDemo {
+    public partial class mainForm : Form {
+        public mainForm() {
             InitializeComponent();
         }
 
-        private void generateButton_Click(object sender, EventArgs e)
-        {
+        private void generateButton_Click(object sender, EventArgs e) {
             String[] words = "\r\n \r\n bla la dada kaba bum bam tam lam sam ham baba papa mama baba saka daka puki muki nea tam net moi tebe mne".Split(' ');
             String[] punctuation = ". . . . . ! ?".Split(' ');
 
@@ -20,22 +14,18 @@ namespace WinFormsAppDemo
             String blaText = "";
             Boolean theFirstWordFlag = true;
 
-            while (textLength > 0)
-            {
+            while (textLength > 0) {
                 textLength--;
                 String randomWord = words[rnd.Next(0, last)];
-                if (theFirstWordFlag && randomWord.Trim().Length > 1)
-                {
+                if (theFirstWordFlag && randomWord.Trim().Length > 1) {
                     randomWord = randomWord.Substring(0, 1).ToUpper() + randomWord.Substring(1);
                     theFirstWordFlag = false;
                 }
                 String space = " ";
-                if (randomWord == "\r\n")
-                {
+                if (randomWord == "\r\n") {
                     space = "";
                     theFirstWordFlag = true;
-                    if (!punctuation.Contains(blaText.Trim().Last().ToString()))
-                    {
+                    if (!punctuation.Contains(blaText.Trim().Last().ToString())) {
                         blaText = blaText.Trim() + punctuation[rnd.Next(punctuation.Length)];
                     }
                 }
