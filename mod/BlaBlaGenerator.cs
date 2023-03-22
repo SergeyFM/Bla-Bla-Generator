406265061c1c595ea54aca85fb8247bf346a96c2
SergeyFM <github@svistunov.su>
2023-03-22 14:23:12 +0300

    Updated the Bla-bla generation algorithm

--- mod/BlaBlaGenerator.cs
+++ mod/BlaBlaGenerator.cs
@@ -1,6 +1,6 @@
 ﻿namespace WinFormsAppDemo.mod {
     internal class BlaBlaGenerator {
-        private static String[] words = "\r\n \r\n bla la dada kaba bum bam tam lam sam ham baba papa mama baba saka daka puki muki nea tam net moi tebe mne kriba lata krum blakadasa malaka taduka bik pra eiru takuk tra bit tra prita dla".Split(' '); 
+        private static String[] words = "\r\n \r\n bla la dada kaba bum bam tam lam sam ham baba papa mama baba saka daka puki muki nea tam net moi tebe mne kriba lata krum bakadasa malaka taduka bik pra eiru takuk tra bit tra prita dla talla pikula mlita".Split(' '); 
         public String Generate() {
 
             String[] punctuation = ". . . . . ! ?".Split(' ');
@@ -11,12 +11,16 @@
             String blaText = "";
             Boolean theFirstWordFlag = true;
             int currentLineLength = 0;
+            int currentLinesAmount = 0;
 
             while (textLength > 0) {
                 textLength--;
                 String randomWord = words[rnd.Next(0, last)];
                 currentLineLength += randomWord.Length;
-                if (currentLineLength > 80) randomWord = "\r\n";
+                if (currentLineLength > 80) {
+                    randomWord = "\r\n";
+                    currentLinesAmount++;
+                }
                 if (theFirstWordFlag && randomWord.Trim().Length > 1) {
                     randomWord = randomWord.Substring(0, 1).ToUpper() + randomWord.Substring(1);
                     theFirstWordFlag = false;
@@ -37,7 +41,14 @@
                     currentLineLength = 0;
                 }
 
-                blaText += randomWord + space;
+                if (currentLinesAmount >= 4 && rnd.Next(1, 2) == 1) {
+                    space += "\r\n";
+                    currentLinesAmount = 0;
+                }
+
+                if (textLength == 0 && !punctuation.Contains((blaText + " ").Trim().Last().ToString())) space = ".";
+
+                    blaText += randomWord + space;
             }
             return blaText;
         }
