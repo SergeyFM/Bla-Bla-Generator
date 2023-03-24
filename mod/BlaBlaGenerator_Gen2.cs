40cbd23fe9a88b6dee022397fcfcf4689a148f81
SergeyFM <github@svistunov.su>
2023-03-24 13:46:30 +0300

    Development of next gen Bla-bla generator

---
+++ mod/BlaBlaGenerator_Gen2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsAppDemo.mod {
+    internal class BlaBlaGenerator_Gen2 {
+        // list of the most used words in the English language
+        private static readonly String[] words =  {
+            "the", "be", "to", "of", "and", "a", "in", "that", "have", "I", "it", "for", "not", "on", "with", "he", "as", "you", "do", "at", "this", "but", "his", "by", "from", "they", "we", "say", "her", "she", "or", "an", "will", "my", "one", "all", "would", "there", "their", "what", "so", "up", "out", "if", "about", "who", "get", "which", "go", "me", "when", "make", "can", "like", "time", "no", "just", "him", "know", "take", "people", "into", "year", "your", "good", "some", "could", "them", "see", "other", "than", "then", "now", "look", "only", "come", "its", "over", "think", "also", "back", "after", "use", "two", "how", "our", "work", "first", "well", "way", "even", "new", "want", "because", "any", "these", "give", "day", "most", "us"
+        };
+        // list of the most popular names in the English language
+        private static readonly String[] names = {
+            "James", "John", "Robert", "Michael", "William", "David", "Richard", "Charles", "Joseph", "Thomas", "Christopher", "Daniel", "Paul", "Mark", "Donald", "George", "Kenneth", "Steven", "Edward", "Brian", "Ronald", "Anthony", "Kevin", "Jason", "Matthew", "Gary", "Timothy", "Jose", "Larry", "Jeffrey", "Frank", "Scott", "Eric", "Stephen", "Andrew", "Raymond", "Gregory", "Joshua", "Jerry", "Dennis", "Walter", "Patrick", "Peter", "Harold", "Douglas", "Henry", "Carl", "Arthur", "Ryan", "Roger"
+        };
+        // list of the most popular tourist destinations
+        private static readonly String[] destinations = {
+            "Paris", "London", "Rome", "New York", "Barcelona", "Berlin", "Madrid", "Prague", "Vienna", "Amsterdam", "Brussels", "Budapest", "Warsaw", "Dublin", "Copenhagen", "Stockholm", "Athens", "Edinburgh", "Oslo", "Lisbon", "Reykjavik", "Moscow", "Beijing", "Tokyo", "Hong Kong", "Bangkok", "Singapore", "Sydney", "Melbourne", "Perth", "Auckland", "Cape Town", "Rio de Janeiro", "Buenos Aires", "Santiago", "Mexico City", "Lima", "Bogota", "Caracas", "Belize City", "San Jose", "San Salvador", "Managua", "Panama City", "Havana", "Santo Domingo", "Kingston", "Port-au-Prince", "Nassau", "San Juan", "Bridgetown", "Port of Spain", "Castries", "Kingstown", "Roseau", "Saint George's", "Saint John's", "Basseterre", "Charlotte Amalie", "Willemstad", "Oranjestad" };
+        
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
