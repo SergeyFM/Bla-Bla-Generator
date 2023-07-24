cae3054795f507c5c67cdefd824bfbc8114ebe38
SergeyFM <github@svistunov.su>
2023-07-24 12:19:46 +0300

    better deal with the database

--- mod/db.cs
+++ mod/db.cs
@@ -1,21 +1,27 @@
 ﻿using LiteDB;
 
-namespace WinFormsAppDemo.mod {
-    internal class db {
-        public LiteDatabase DB;
-        public db() {
-            DB = new(@"data.db");
+namespace WinFormsAppDemo.mod;
+internal class db {
+    //public LiteDatabase DB;
+    public db() {
+        //DB = new(@"data.db");
 
-        }
-        public void Save(DbData obj) {
-            var data = DB.GetCollection<DbData>();
+    }
+    public void Save(DbData obj) {
+        using (LiteDatabase DB = new(@"data.db")) {
+            var data = DB.GetCollection<DbData>("theData");
             data.DeleteAll();
             data.Insert(obj);
         }
-        public String Load() {
-            var data = DB.GetCollection<DbData>();
+    }
+    public String Load() {
+        using (LiteDatabase DB = new(@"data.db")) {
+
+            var data = DB.GetCollection<DbData>("theData");
             DbData[] texts = data.FindAll().ToArray();
+
             String ret = texts.Length > 0 ? texts[0].text : "";
+
             return ret;
         }
     }
